ebcf0b2978862a4f9d3fe77eaa0849951116023e
Rafael Ayala <r.ayala14@imperial.ac.uk>
2020-10-06 16:10:02 +0000

    version 0.99.3

--- inst/doc/NutrienTrackeR.R
+++ inst/doc/NutrienTrackeR.R
@@ -1,13 +1,13 @@
-## ----setup, echo=FALSE---------------------------------------------------
+## ----setup, echo=FALSE--------------------------------------------------------
 knitr::opts_chunk$set(message=FALSE, fig.path='figures/')
 
-## ----tidy = TRUE, eval = FALSE-------------------------------------------
+## ----tidy = TRUE, eval = FALSE------------------------------------------------
 #  install.packages("NutrienTrackeR")
 
-## ----tidy = TRUE, eval = TRUE--------------------------------------------
+## ----tidy = TRUE, eval = TRUE-------------------------------------------------
 library(NutrienTrackeR)
 
-## ----tidy = TRUE---------------------------------------------------------
+## ----tidy = TRUE--------------------------------------------------------------
 # USDA dataset
 USDA_dataset <- food_composition_data$USDA
 
@@ -17,12 +17,12 @@ CIQUAL_dataset <- food_composition_data$CIQUAL
 # BEDCA dataset
 BEDCA_dataset <- food_composition_data$BEDCA
 
-## ----tidy = TRUE---------------------------------------------------------
+## ----tidy = TRUE--------------------------------------------------------------
 # Get nutrients included in the USDA dataset
 nutrients_USDA <- getNutrientNames(food_database = "USDA")
 print(head(nutrients_USDA), 4)
 
-## ----tidy = TRUE, tidy.opts=list(indent = 2, width.cutoff = 150)---------
+## ----tidy = TRUE, tidy.opts=list(indent = 2, width.cutoff = 150)--------------
 # Top 2 high-protein CIQUAL foods
 subsetFoodRichIn(nutrient_name = "Protein (g)", food_database = "CIQUAL", n = 2)[, "food_name"]
 
@@ -30,11 +30,11 @@ subsetFoodRichIn(nutrient_name = "Protein (g)", food_database = "CIQUAL", n = 2)
 subsetFoodRichIn(nutrient_name = "Protein (g)", food_database = "BEDCA", food_group = "Fruits and fruit products", n = 3)[, "food_name"]
 
 
-## ----tidy = TRUE, tidy.opts=list(indent = 2, width.cutoff = 150)---------
+## ----tidy = TRUE, tidy.opts=list(indent = 2, width.cutoff = 150)--------------
 # Find the USDA food name "Tomatoes, green, raw"
 findFoodName(keywords = c("Tomato", "raw"), food_database = "USDA")
 
-## ----tidy = TRUE, tidy.opts=list(indent = 2, width.cutoff = 150)---------
+## ----tidy = TRUE, tidy.opts=list(indent = 2, width.cutoff = 150)--------------
 # Foods eaten in day 1
 head(sample_diet_USDA[[1]])
 
@@ -43,7 +43,7 @@ head(sample_diet_USDA[[1]])
 daily_intake <- dietBalance(my_daily_food = sample_diet_USDA, food_database = "USDA", 
                            age = 27, gender = "female")
 
-## ----tidy = TRUE, results='asis', fig.pos = "center"---------------------
+## ----tidy = TRUE, results='asis', fig.pos = "center"--------------------------
 nutrientIntakePlot(daily_intake)
 
 ## ----tidy = TRUE, tidy.opts=list(indent = 2, width.cutoff = 180), results='asis', fig.pos = "center",  fig.width = 20, fig.height = 10----
